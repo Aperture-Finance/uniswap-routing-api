3df82f14634ad92452968b3117682044865c3506
Peiqian Li <peiqian@aperture.finance>
2023-10-11 13:40:54 -0700

    Lower on-chain quote provider gas limit.

--- lib/handlers/injector-sor.ts
+++ lib/handlers/injector-sor.ts
@@ -274,17 +274,17 @@ export abstract class InjectorSOR<Router, QueryParams> extends Injector<
                   maxTimeout: 1000,
                 },
                 {
-                  multicallChunk: 110,
+                  multicallChunk: 24,
                   gasLimitPerCall: 1_200_000,
                   quoteMinSuccessRate: 0.1,
                 },
                 {
-                  gasLimitOverride: 3_000_000,
-                  multicallChunk: 45,
+                  gasLimitOverride: 1_000_000,
+                  multicallChunk: 24,
                 },
                 {
-                  gasLimitOverride: 3_000_000,
-                  multicallChunk: 45,
+                  gasLimitOverride: 1_000_000,
+                  multicallChunk: 24,
                 },
                 {
                   baseBlockOffset: -25,
